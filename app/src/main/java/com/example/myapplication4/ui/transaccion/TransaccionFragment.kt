cdadc75d6bb6bd235cc300288b480cdab7f01042
spoleschiNB <spoleschi@gmail.com>
2024-10-08 21:11:01 -0300

    Modificación en transaccion

--- app/src/main/java/com/example/myapplication4/ui/transaccion/TransaccionFragment.kt
+++ app/src/main/java/com/example/myapplication4/ui/transaccion/TransaccionFragment.kt
@@ -1,203 +1,76 @@
 package com.example.myapplication4.ui.transaccion
 
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.myapplication4.adapters.CategoryAdapter
-import com.example.myapplication4.databinding.FragmentTransaccionBinding
-import com.google.android.material.snackbar.Snackbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication4.databinding.FragmentTransactionBinding
+import com.example.myapplication4.repository.TransactionRepository
+import com.example.myapplication4.adapters.TransactionAdapter
 import com.google.android.material.tabs.TabLayout
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
-class TransaccionFragment : Fragment() {
+class TransactionFragment : Fragment() {
 
-    private var _binding: FragmentTransaccionBinding? = null
+    private var _binding: FragmentTransactionBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewModel: TransaccionViewModel
-    private lateinit var categoryAdapter: CategoryAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentTransaccionBinding.inflate(inflater, container, false)
-        viewModel = ViewModelProvider(this).get(TransaccionViewModel::class.java)
+    private val viewModel: TransactionViewModel by viewModels {
+        TransactionViewModelFactory(TransactionRepository())
+    }
+    private lateinit var adapter: TransactionAdapter
 
-//        descGasto = binding.inputDesc
-//        montoGasto = binding.etAmount
-//        fechaGasto = binding.etDate
-//        numeroCuotas = binding.etCuotas
-//        tasaInteres = binding.etInteres
-//        botonGuardarGasto = binding.btnAdd
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentTransactionBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupRecyclerView()
         setupTabLayout()
-        setupCategoryRecyclerView()
-        setupAmountInput()
-        setupDateInput()
-        setupInstallmentsInput()
-        setupInterestRateInput()
-        setupAddButton()
+        setupPeriodButtons()
+        observeViewModel()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = TransactionAdapter()
+        binding.transactionRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = this@TransactionFragment.adapter
+        }
     }
 
     private fun setupTabLayout() {
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                // Cambiar entre gastos e ingresos
                 when (tab?.position) {
-                    0 -> {
-                        viewModel.setTransactionType(TransactionType.EXPENSE)
-                        viewModel.showExpenseCategories()
-                        binding.tilCuotas.visibility = View.VISIBLE
-                        binding.tilInteres.visibility = View.VISIBLE
-                    }
-
-                    1 -> {
-                        viewModel.setTransactionType(TransactionType.INCOME)
-                        viewModel.showIncomeCategories()
-                        binding.tilCuotas.visibility = View.GONE
-                        binding.tilInteres.visibility = View.GONE
-                    }
+                    0 -> viewModel.setTransactionType(TransactionType.EXPENSE)
+                    1 -> viewModel.setTransactionType(TransactionType.INCOME)
                 }
             }
-
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
     }
 
-    private fun setupCategoryRecyclerView() {
-        categoryAdapter = CategoryAdapter { category ->
-            viewModel.setSelectedCategory(category)
+    private fun setupPeriodButtons() {
+        binding.dayButton.setOnClickListener { viewModel.setPeriod(Period.DAY) }
+        binding.weekButton.setOnClickListener { viewModel.setPeriod(Period.WEEK) }
+        binding.monthButton.setOnClickListener { viewModel.setPeriod(Period.MONTH) }
+        binding.yearButton.setOnClickListener { viewModel.setPeriod(Period.YEAR) }
+        binding.allButton.setOnClickListener { viewModel.setPeriod(Period.ALL) }
+    }
+
+    private fun observeViewModel() {
+        viewModel.transactions.observe(viewLifecycleOwner) { transactions ->
+            adapter.submitList(transactions)
         }
-        binding.rvCategories.apply {
-            layoutManager = GridLayoutManager(context, 4)
-            adapter = categoryAdapter
-        }
-
-        // Observar cambios en las categorías
-        viewModel.currentCategories.observe(viewLifecycleOwner) { categories ->
-            categoryAdapter.submitList(categories)
-        }
-    }
-
-    private fun setupAmountInput() {
-        binding.etAmount.addTextChangedListener { editable ->
-            editable?.toString()?.let { amount ->
-                if (amount.isNotEmpty()) {
-                    viewModel.setAmount(amount.toDouble())
-                }
-            }
-        }
-    }
-
-    private fun setupDateInput() {
-        binding.etDate.setOnClickListener { showDatePickerDialog() }
-
-        viewModel.selectedDate.observe(viewLifecycleOwner) { calendar ->
-            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-            binding.etDate.setText(dateFormat.format(calendar.time))
-        }
-    }
-
-    private fun setupInstallmentsInput() {
-        binding.etCuotas.addTextChangedListener { editable ->
-            editable?.toString()?.toIntOrNull()?.let { installments ->
-                viewModel.setInstallments(installments)
-            }
-        }
-    }
-
-    private fun setupInterestRateInput() {
-        binding.etInteres.addTextChangedListener { editable ->
-            editable?.toString()?.toDoubleOrNull()?.let { rate ->
-                viewModel.setInterestRate(rate)
-            }
-        }
-    }
-
-    private fun showDatePickerDialog() {
-        val calendar = viewModel.selectedDate.value ?: Calendar.getInstance()
-        DatePickerDialog(
-            requireContext(),
-            { _, year, month, dayOfMonth ->
-                viewModel.setSelectedDate(year, month, dayOfMonth)
-            },
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)
-        ).show()
-    }
-
-    private fun setupAddButton() {
-//        botonGuardarGasto.setOnClickListener {
-        binding.btnAdd.setOnClickListener {
-//            val descripcion = descGasto.text.toString()
-//            val montoString = montoGasto.text.toString()
-//            val fechaString = fechaGasto.text.toString()
-//            val cuotasString = numeroCuotas.text.toString()
-//            val interesString = tasaInteres.text.toString()
-
-            val amount = binding.etAmount.text.toString().toDoubleOrNull()
-            val comment = binding.tilComment.editText?.text.toString()
-            val date = viewModel.selectedDate.value
-            val installments = binding.etCuotas.text.toString().toIntOrNull() ?: 1
-            val interestRate = binding.etInteres.text.toString().toDoubleOrNull() ?: 0.0
-
-            //if (descripcion.isNotBlank() && montoString.isNotBlank() && fechaString.isNotBlank() && cuotasString.isNotBlank() && interesString.isNotBlank()) {
-
-            if (amount != null && comment.isNotBlank() && viewModel.selectedCategory.value != null && date != null) {
-                viewModel.addTransaction(amount, comment, date, installments, interestRate)
-                clearFields()
-                showSuccessMessage()
-                requireActivity().supportFragmentManager.popBackStack() // Go back to previous fragment
-            } else {
-                showErrorMessage()
-            }
-        }
-    }
-
-    private fun clearFields() {
-        binding.etAmount.text.clear()
-        binding.tilComment.editText?.text?.clear()
-        binding.etCuotas.text?.clear()
-        binding.etInteres.text?.clear()
-        viewModel.setSelectedDate(
-            Calendar.getInstance().get(Calendar.YEAR),
-            Calendar.getInstance().get(Calendar.MONTH),
-            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        )
-    }
-
-    private fun showSuccessMessage() {
-        Snackbar.make(binding.root, "Transacción añadida con éxito", Snackbar.LENGTH_SHORT).show()
-    }
-
-    private fun showErrorMessage() {
-//        if (monto == null) montoGasto.error = "Monto inválido"
-//        if (cuotas == null) numeroCuotas.error = "Número de cuotas inválido"
-//        if (interes == null) tasaInteres.error = "Tasa de interés inválida"
-//        if (descripcion.isBlank()) descGasto.error = "Descripción requerida"
-//        if (montoString.isBlank()) montoGasto.error = "Monto requerido"
-//        if (fechaString.isBlank()) fechaGasto.error = "Fecha requerida"
-//        if (cuotasString.isBlank()) numeroCuotas.error = "Número de cuotas requerido"
-//        if (interesString.isBlank()) tasaInteres.error = "Tasa de interés requerida"
-        Snackbar.make(binding.root, "Por favor, completa todos los campos", Snackbar.LENGTH_SHORT)
-            .show()
     }
 
     override fun onDestroyView() {
@@ -205,3 +78,4 @@ class TransaccionFragment : Fragment() {
         _binding = null
     }
 }
+
--- app/src/main/java/com/example/myapplication4/ui/transaccion/TransaccionViewModel.kt
+++ app/src/main/java/com/example/myapplication4/ui/transaccion/TransaccionViewModel.kt
@@ -3,170 +3,69 @@ package com.example.myapplication4.ui.transaccion
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.myapplication4.Clases.Categoria
-import com.example.myapplication4.Clases.Gasto
-import com.example.myapplication4.Clases.Ingreso
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.myapplication4.Clases.Transaccion
-import java.util.Calendar
+import com.example.myapplication4.repository.TransactionRepository
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
-class TransaccionViewModel : ViewModel() {
+class TransactionViewModel(private val repository: TransactionRepository) : ViewModel() {
 
-//    private val _categories = MutableLiveData<List<Categoria>>()
-//    val categories: LiveData<List<Categoria>> = _categories
+    private val _transactions = MutableLiveData<List<Transaccion>>()
+    val transactions: LiveData<List<Transaccion>> = _transactions
 
-    private val _expenseCategories = MutableLiveData<List<Categoria>>()
-    val expenseCategories: LiveData<List<Categoria>> = _expenseCategories
-
-    private val _incomeCategories = MutableLiveData<List<Categoria>>()
-    val incomeCategories: LiveData<List<Categoria>> = _incomeCategories
-
-    private val _currentCategories = MutableLiveData<List<Categoria>>()
-    val currentCategories: LiveData<List<Categoria>> = _currentCategories
-
-    private val _selectedCategory = MutableLiveData<Categoria>()
-    val selectedCategory: LiveData<Categoria> = _selectedCategory
-
-    private val _transactionType = MutableLiveData<TransactionType>()
-    val transactionType: LiveData<TransactionType> = _transactionType
-
-    private var currentType: String = "Gasto"
-
-    private val _amount = MutableLiveData<Double>()
-    val amount: LiveData<Double> = _amount
-
-    private val _selectedDate = MutableLiveData<Calendar>()
-    val selectedDate: LiveData<Calendar> = _selectedDate
-
-    private val _installments = MutableLiveData<Int>()
-    val installments: LiveData<Int> = _installments
-
-    private val _interestRate = MutableLiveData<Double>()
-    val interestRate: LiveData<Double> = _interestRate
-
-    private val _transactions = MutableLiveData<MutableList<Transaccion>>()
-    val transactions: LiveData<MutableList<Transaccion>> = _transactions
-
+    private var currentType = TransactionType.EXPENSE
+    private var currentPeriod = Period.ALL
 
     init {
-        loadInitialCategories()
-        _transactionType.value = TransactionType.EXPENSE
-        _selectedDate.value = Calendar.getInstance() // Inicializar con la fecha actual
-        _transactions.value = mutableListOf()
-    }
-
-    private fun loadInitialCategories() {
-        val initialExpenseCategories = listOf(
-            Categoria(1, "Alimentos", "", "#3EC54A", "Gasto"),
-            Categoria(2, "Trasporte", "", "#FFEB3C", "Gasto"),
-            Categoria(3, "Ocio", "", "#800080", "Gasto"),
-            Categoria(4,"Salud", "", "#FF0000","Gasto"),
-            Categoria(5,"Casa", "", "#287FD2","Gasto"),
-            Categoria(6,"Educación","", "#FF00FF","Gasto"),
-            Categoria(7,"Regalos","", "#00FFFF","Gasto")
-        )
-        _expenseCategories.value = initialExpenseCategories
-
-        val initialIncomeCategories = listOf(
-            Categoria(8, "Salario", "", "#808080", "Ingreso"),
-            Categoria(9, "Inversiones", "", "#FF9300", "Ingreso")
-        )
-        _incomeCategories.value = initialIncomeCategories
-
-        // Inicialmente mostramos las categorías de gasto
-        _currentCategories.value = initialExpenseCategories
+        loadTransactions()
     }
 
     fun setTransactionType(type: TransactionType) {
-        _transactionType.value = type
+        currentType = type
+        loadTransactions()
     }
 
-    fun showExpenseCategories() {
-        currentType = "Gasto"
-        _currentCategories.value = _expenseCategories.value
+    fun setPeriod(period: Period) {
+        currentPeriod = period
+        loadTransactions()
     }
 
-    fun showIncomeCategories() {
-        currentType = "Ingreso"
-        _currentCategories.value = _incomeCategories.value
-    }
+    private fun loadTransactions() {
+        viewModelScope.launch {
+            val allTransactions = when (currentType) {
+                TransactionType.EXPENSE -> repository.getExpenses()
+                TransactionType.INCOME -> repository.getIncomes()
+            }
 
-    fun setSelectedDate(year: Int, month: Int, dayOfMonth: Int) {
-        val calendar = Calendar.getInstance().apply {
-            set(Calendar.YEAR, year)
-            set(Calendar.MONTH, month)
-            set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            val filteredTransactions = when (currentPeriod) {
+                Period.DAY -> allTransactions.filter { it.fecha == LocalDate.now() }
+                Period.WEEK -> allTransactions.filter { it.fecha.isAfter(LocalDate.now().minusWeeks(1)) }
+                Period.MONTH -> allTransactions.filter { it.fecha.isAfter(LocalDate.now().minusMonths(1)) }
+                Period.YEAR -> allTransactions.filter { it.fecha.isAfter(LocalDate.now().minusYears(1)) }
+                Period.ALL -> allTransactions
+            }
+
+            _transactions.postValue(filteredTransactions)
         }
-        _selectedDate.value = calendar
-    }
-
-    fun setSelectedCategory(category: Categoria) {
-        _selectedCategory.value = category
-    }
-
-    fun setAmount(amount: Double) {
-        _amount.value = amount
-    }
-
-    fun setInstallments(installments: Int) {
-        _installments.value = installments
-    }
-
-    fun setInterestRate(rate: Double) {
-        _interestRate.value = rate
-    }
-
-//    fun agregarGasto(cantCuotas: Int, interes: Float, desc: String, monto: Float, fecha: LocalDate, categoria: Categoria) {
-//        if (cantCuotas < 1 || cantCuotas > 24) {
-//            throw IllegalArgumentException("La cantidad de cuotas permitidas es: minimo 1, maximo 24")
-//        }
-//        val newGasto = Gasto(
-//            cantCuotas = cantCuotas,
-//            interes = interes,
-//            idGasto = id++,
-//            descGasto = desc,
-//            montoGasto = monto,
-//            fechaGasto = fecha,
-//            categoriaGasto = categoria
-//        )
-//        val updatedExpenses = _gastos.value.orEmpty() + newGasto
-//        _gastos.value = updatedExpenses
-//    }
-
-    fun addTransaction(amount: Double, comment: String, date: Calendar, installments: Int, interestRate: Double) {
-        val category = _selectedCategory.value ?: return
-        val transactionType = _transactionType.value ?: return
-
-        val transaction = when (transactionType) {
-            TransactionType.EXPENSE -> Gasto(
-                idGasto = generateId(),
-                descGasto = comment,
-                montoGasto = amount.toFloat(),
-                fechaGasto = LocalDate.of(date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1, date.get(Calendar.DAY_OF_MONTH)),
-                categoriaGasto = category,
-                cantCuotas = installments,
-                interes = interestRate.toFloat()
-            )
-            TransactionType.INCOME -> Ingreso(
-                idIngreso = generateId(),
-                descIngreso = comment,
-                montoIngreso = amount.toFloat(),
-                fechaIngreso = LocalDate.of(date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1, date.get(Calendar.DAY_OF_MONTH)),
-                categoriaIngreso = category
-            )
-        }
-
-        val currentTransactions = _transactions.value ?: mutableListOf()
-        currentTransactions.add(transaction)
-        _transactions.value = currentTransactions
-    }
-
-    private fun generateId(): Int {
-        // Simple ID generation. In a real app, you'd use a more robust method.
-        return (_transactions.value?.size ?: 0) + 1
     }
 }
 
-enum class TransactionType {
-    EXPENSE, INCOME
+//enum class TransactionType {
+//    EXPENSE, INCOME
+//}
+
+enum class Period {
+    DAY, WEEK, MONTH, YEAR, ALL
+}
+
+class TransactionViewModelFactory(private val repository: TransactionRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TransactionViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TransactionViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
